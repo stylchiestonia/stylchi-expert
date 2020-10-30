1f9097b9b771685495b5ba690b368c9d230bb05a
tenovos-mahnoor <mahnoor@tenovos.com>
2020-10-30 15:48:11 +0500

    updated url

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ import 'assets/css/nucleo-icons.css';
 import 'react-datetime/css/react-datetime.css';
 
 const hist = createBrowserHistory();
-axios.defaults.baseURL = ' http://localhost:5000/api'
+axios.defaults.baseURL = 'https://stylchi-backend-293213.ew.r.appspot.com/api'
 
 if (localStorage.jwtToken) {
   const token = localStorage.jwtToken;

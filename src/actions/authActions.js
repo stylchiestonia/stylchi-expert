9aaa05dd9b17d47f342dfd0d3896727104e9f981
tenovos-mahnoor <mahnoor@tenovos.com>
2020-10-30 12:45:42 +0500

    updated auth

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import setAuthToken from "utils/setAuthToken";
 import jwt_decode from "jwt-decode";
 
-import { GET_ERRORS, SET_CURRENT_USER, USER_LOADING, USER_REGISTERED} from "./types";
+import { GET_ERRORS, SET_CURRENT_USER, USER_LOADING} from "./types";
 
 
 export const registerUser = (userData, history) => dispatch => {
@@ -12,12 +12,7 @@ export const registerUser = (userData, history) => dispatch => {
   axios
     .post("/register", userData)
     .then(res => 
-      {dispatch({
-        type: USER_REGISTERED
-      })}
-      ).then(
-        history.push("/thank-you")
-      ).catch(err =>
+      history.push("/thank-you")).catch(err =>
       dispatch({
         type: GET_ERRORS,
         payload: err.response.data

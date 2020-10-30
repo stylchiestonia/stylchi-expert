847691167a8b3c68e44d5c37c1b7dfce930c9edc
tenovos-mahnoor <mahnoor@tenovos.com>
2020-10-30 05:41:40 +0500

    login to settings

--- src/views/Login.js
+++ src/views/Login.js
@@ -29,13 +29,13 @@ class Login extends React.Component {
   componentDidMount() {
     // If logged in and user navigates to Login page, should redirect them to dashboard
     if (this.props.auth.isAuthenticated) {
-      this.props.history.push("/admin/dashboard");
+      this.props.history.push("/admin/settings");
     }
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.auth.isAuthenticated) {
-      this.props.history.push("/admin/dashboard");
+      this.props.history.push("/admin/settings");
     }
 
     if (nextProps.errors) {
